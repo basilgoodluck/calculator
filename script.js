dfd4404b46f73e0709bf29abbe81eec59d600cb3
Basil Goodluck <basilgoodluck22@gmail.com>
2024-01-03 17:05:59 +0100

    Updated

--- script.js
+++ script.js
@@ -5,13 +5,35 @@ const operaBtn = document.querySelectorAll('.operaBtn')
 const clearBtn = document.querySelector('.clearBtn')
 const deleteBtn = document.querySelector('.deleteBtn')
 const prevScreen = document.querySelector('prev-operand')
-const currentScreen = document.querySelector('')
+const currentScreen = document.querySelector('.current-operand')
 
-class Calculator {
-    constructor (prevScreen, currentScreen) {
-        this
+
+
+
+const calculator = {
+
+    numBtn : document.querySelectorAll('.numBtn'),
+    operaBtn : document.querySelectorAll('.operaBtn'),
+    clearBtn : document.querySelector('.clearBtn'),
+    deleteBtn : document.querySelector('.deleteBtn'),
+    prevScreen : document.querySelector('.prev-operand'),
+    currentScreen : document.querySelector('.current-operand'),
+
+    display () {
+        numBtn.forEach((num)=>{
+            num.addEventListener('click', ()=>{
+                var prevScreen = document.getElementById('prev-operand');
+                console.log(typeof prevScreen)
+
+                prevScreen.innerHTML += num.innerHTML
+            })
+        })
+    },
+
+    calculator () {
+
     }
+      
 }
 
-
-const calculator new calculato
+calculator.display()
