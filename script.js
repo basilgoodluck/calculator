fdc85bfaff45878747b1a9521983298cb50650af
Basil Goodluck <basilgoodluck22@gmail.com>
2024-01-03 13:43:12 +0100

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+
+
 const numBtn = document.querySelectorAll('.numBtn')
+const operaBtn = document.querySelectorAll('.operaBtn')
 const clearBtn = document.querySelector('.clearBtn')
 const deleteBtn = document.querySelector('.deleteBtn')
+const prevScreen = document.querySelector('prev-operand')
+const currentScreen = document.querySelector('')
+
+class Calculator {
+    constructor (prevScreen, currentScreen) {
+        this
+    }
+}
 
 
-
+const calculator new calculato
