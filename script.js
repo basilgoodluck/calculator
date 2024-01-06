060016a064094ddc13690c1df36377cb2c73eed3
Basil Goodluck <basilgoodluck22@gmail.com>
2024-01-06 13:48:02 +0100

    Updated

--- script.js
+++ script.js
@@ -8,30 +8,40 @@ const calculator = {
     deleteBtn : document.querySelector('.deleteBtn'),
     prevScreen : document.querySelector('.prev-operand'),
     currentScreen : document.querySelector('.current-operand'),
+    equation : '',
+    initialNum : '',
+    finalNum : '',
+    
+
 
     display () {
 
         let accumulatedNumbers = ''; 
-        let initialNum = '';
-        let finalNum = '';
         this.numBtn.forEach((num)=>{
             num.addEventListener('click', ()=>{
-                
                 accumulatedNumbers += num.innerHTML
-
-                this.prevScreen.innerHTML = accumulatedNumbers;
-
+                this.prevScreen.innerHTML = accumulatedNumbers;gi
             })
         })
 
         this.operaBtn.forEach((opera) => {
             opera.addEventListener('click', ()=>{
                 initialNum = accumulatedNumbers
-                
+                this.equation += initialNum
                 accumulatedNumbers = ''
-                
+
+                this.numBtn.forEach((num) => {
+                    num.addEventListener('click', ()=>{
+                        this.finalNum += num.textContent
+                        this.equation += this.finalNum
+                        
+                    })
+                })
+
             })
         })
+        console.log(this.equation += this.initialNum)
+        
     }   
 }
 
