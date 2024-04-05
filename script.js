6091cd345834e6de74eb1766b9fd0a8fabcdeead
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-05 14:55:48 +0100

    JS Updated

--- script.js
+++ script.js
@@ -5,26 +5,87 @@ class Calculator {
         this.clear()
     }
 
-    clear(){    
+    clear(){  
+        this.prevOperand = ''  
         this.currentOperand = ''
-        this.prevOperand = ''   
         this.operation = undefined 
 
     }
     appendNumber(number){
+        if((number == '.') && (this.currentOperand.includes('.'))) return
         this.currentOperand = this.currentOperand.toString() + number.toString() 
     }
     chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.prevOperand !== ''){
+            this.compute()
+        }
+        this.operation= operation
+        this.prevOperand = this.currentOperand
+        this.currentOperand = ''
+        
 
     }
     compute(){
+        let computation
+        const prev = parseFloat(this.prevOperand)
+        const current = parseFloat(this.currentOperand)
 
+        if(isNaN(prev) || isNaN(current)) return
+        
+        switch(this.operation){
+            case '+' :
+                computation = prev + current
+                break
+            case '-' :
+                computation = prev - current
+                break
+            case '*' :
+                computation = prev * current
+                break
+            case '/' :
+                computation = prev / current
+                break
+            default :
+                return
+        }
+
+        this.currentOperand = computation
+        this.prevOperand = ''
+        this.operation = undefined
+            
+            
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        }else{
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else{
+            return integerDisplay
+        }
+        
     }
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.prevOperandTextElement.innerText = 
+                `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`
+        }
+        else{
+            this.prevOperandTextElement.innerText = ''
+        }
     }
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1) 
     }
 }
 
@@ -44,4 +105,26 @@ numBTNs.forEach(btn => {
         calculator.appendNumber(btn.innerText)
         calculator.updateDisplay()
     })
+})
+
+operaBTNs.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        calculator.chooseOperation(btn.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalBtn.addEventListener('click', ()=>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearBtn.addEventListener('click', ()=>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteBtn.addEventListener('click', ()=>{
+    calculator.delete()
+    calculator.updateDisplay()
 })
