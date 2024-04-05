24d067e097ca215b124da5c37c2ea56a8acce220
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-05 13:08:05 +0100

    JS Updated

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-window.addEventListener('DOMContentLoaded', function(){
-
-    const screen = document.getElementById('screen')
-    let prevOperand = document.getElementById('prev-operand')
-    const currentOperand = document.getElementById('current-operand')
-    const numbers = document.querySelectorAll('.numBtn')
-    const algebraBTNs = document.querySelectorAll('.operaBtn')
-    const period = document.getElementById('pBtn')
-
-    function allCLear(){
+class Calculator {
+    constructor(prevOperandTextElement, currentOperandTextElement){
+        this.prevOperandTextElement = prevOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
-    function deleteNum(){
-    }
-
-    function appendNumber(num){  
-    }
-
-    function updateDisplay(){
+    clear(){    
+        this.currentOperand = ''
+        this.prevOperand = ''   
+        this.operation = undefined 
 
     }
-
-    function chooseOperation(){
+    appendNumber(number){
+        this.currentOperand = this.currentOperand.toString() + number.toString() 
+    }
+    chooseOperation(operation){
 
     }
-    
-    function calculate(){
+    compute(){
 
     }
-     
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+    delete(){
+
+    }
+}
+
+
+const prevOperandTextElement = document.getElementById('prev-operand')
+const currentOperandTextElement = document.getElementById('current-operand')
+const numBTNs = document.querySelectorAll('.numBtn')
+const operaBTNs = document.querySelectorAll('.operaBtn')
+const allClearBtn = document.querySelector('.clearBtn')
+const deleteBtn = document.querySelector('.deleteBtn')
+const equalBtn = document.querySelector('.eBtn')
+
+const calculator = new Calculator(prevOperandTextElement, currentOperandTextElement)
+
+numBTNs.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        calculator.appendNumber(btn.innerText)
+        calculator.updateDisplay()
+    })
 })
